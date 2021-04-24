45c0d30ae7b8a4a23baf57bef2247b649528a00e
Sergio Pallet <sergiopallet@gmail.com>
2021-04-23 21:55:49 -0300

    Adding page home layout

--- podcastr/src/pages/index.tsx
+++ podcastr/src/pages/index.tsx
@@ -1,8 +1,143 @@
+import { GetStaticProps } from 'next';
+import Image  from 'next/image';
+import { type } from 'node:os';
+import { api } from '../services/api';
+import { format, parseISO } from 'date-fns';
+import ptBR from 'date-fns/locale/pt-BR';
+import { converDurationToTimeString } from '../utils/convertDurationToTimeString';
+import styles from "./home.module.scss"
 
 
+type Episodes = {
+  id: string;
+  title: string;
+  thumbnail: string;
+  description: string;
+  members: string;
+  duration: number;
+  durationAsString: string;
+  url: string;
+  publishedAt: string;
+};
 
-export default function Home() {
+type HomeProps = {
+  latestEpisodes: Episodes[],
+  allEpisodes: Episodes[]
+}
+
+export default function Home({ latestEpisodes, allEpisodes }: HomeProps) {
   return (
-    <h3>home</h3>
+    <div className={styles.homepage}>
+      <section className={styles.latestEpisodes}>
+        <h2>ultimos lançamentos</h2>
+        <ul>
+          {latestEpisodes.map(episode => {
+            
+            return (
+              <li key={episode.id}>
+                <Image 
+                width={192} 
+                height={192} 
+                src={episode.thumbnail} 
+                alt={episode.title} 
+                objectFit="cover"
+                />
+                <div className={styles.episodeDetails}>
+                  <a href="">{episode.title}</a>
+                  <p>{episode.members}</p>
+                  <span>{episode.publishedAt}</span>
+                  <span>{episode.durationAsString}</span>
+                </div>
+                <button type="button" > 
+                <img src= "/play-green.svg" alt="tocar episódio"/>
+                </button>
+              </li>
+            )
+          })}
+        </ul>
+      </section>
+      <section className={styles.allEpisodes}>
+          <h2>Todos episódios</h2>
+
+          <table cellSpacing={0}>
+            <thead>
+              <th></th>
+              <th>Podcast</th>
+              <th>Integrantes</th>
+              <th>Data</th>
+              <th>Duração</th>
+              <th></th>
+            </thead>
+            <tbody>
+              {allEpisodes.map(episode => {
+                return (
+                  <tr key={episode.id}>
+                    <td style={{width: 72}}>
+                      <Image
+                        width={120}
+                        height={120}
+                        src={episode.thumbnail}
+                        alt={episode.title}
+                        objectFit="cover"
+                      />
+                    </td>
+                    <td>
+                      <a href="">{episode.title}</a>
+                    </td>
+                    <td>{episode.members}</td>
+                    <td style={{width:100}}>{episode.publishedAt}</td>
+                    <td>{episode.durationAsString}</td>
+                    <td>
+                      <button type="button">
+                        <img src="/play-green.svg" alt="tocar episódio"/>
+                      </button>
+                    </td>
+
+                  </tr>
+                );
+              })}
+            </tbody>
+
+          </table>
+      </section>
+    </div>
   )
+}
+
+export const getStaticProps: GetStaticProps = async () => {
+  const { data } = await api.get('episodes', {
+    params: {
+      _limit: 12,
+      _sort: 'published_at',
+      _order: 'desc'
+    }
+  });
+
+
+
+  const episodes = data.map(episode => ({
+    id: episode.id,
+    title: episode.title,
+    thumbnail: episode.thumbnail,
+    members: episode.members,
+    publishedAt: format(parseISO(episode.published_at), 'd MMM, yy', { locale: ptBR }),
+    duration: Number(episode.file.duration),
+    durationAsString: converDurationToTimeString(Number(episode.file.duration)),
+    description: episode.description,
+    url: episode.file.url
+
+  }))
+
+  const latestEpisodes = episodes.slice(0, 2);
+  const allEpisodes = episodes.slice(2, episodes.length);
+
+
+  // const data = await response.json()
+  return {
+    props: {
+      latestEpisodes,
+      allEpisodes
+    },
+    revalidate: 60 * 60 * 8,
+  };
 }
